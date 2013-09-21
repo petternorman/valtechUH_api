0971acee7b62d33bd22af3db18ddcbc5cd57c662
Petter Norman <petter.norman@valtech.se>
2013-09-21 18:36:49 +0200

    remove unused webapi files

--- TrafficAPI/TrafficAPI/Global.asax.cs
+++ TrafficAPI/TrafficAPI/Global.asax.cs
@@ -20,7 +20,6 @@ namespace TrafficAPI
         {
             AreaRegistration.RegisterAllAreas();
 
-            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
