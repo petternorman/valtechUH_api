4cb1ddcd5459ae658bee93cdb5991c6e564f47e6
Sara Ingmar <sara.ingmar@valtech.se>
2013-09-21 13:04:33 +0200

    Added ticket info model and repository

---
+++ TrafficAPI/TrafficAPI/Models/TicketInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafficAPI.Models
+{
+	public class TicketInfoModel
+	{
+		public double price { get; set; }
+
+		public string msgText { get; set; }
+
+		public string msgNumber { get; set; }
+	}
+}
