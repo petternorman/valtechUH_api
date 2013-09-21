fa15048383428f5aa4ff0c7d6f13ba5f911a5fbf
Petter Norman <petter.norman@valtech.se>
2013-09-21 12:12:01 +0200

    structure

---
+++ TrafficAPI/TrafficAPI/Api/Station.cs
@@ -0,0 +1,11 @@
+﻿using ServiceStack.ServiceHost;
+
+namespace TrafficAPI.Api
+{
+    [Route("/station")]
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TrafficAPI/TrafficAPI/Api/StationService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack.ServiceInterface;
+using TrafficAPI.Api.ResponseModels;
+
+namespace TrafficAPI.Api
+{
+    public class StationService : Service
+    {
+        public object Get(Station request)
+        {
+            return new StationResponse {Result = "station"};
+        }
+    }
+}
